0b32216af22c6bcdb4b81022f1a1c043b5cb02d6
Chad Ostrowski <hi@chadoh.com>
2018-01-07 10:43:57 -0500

    Tests: Fill in and fix
    Without using the 'done' syntax from Mocha which provides asynchronous
    tests, none of the assertions written inside the `awaitReRender` blocks
    were even being called!
    
    This fixes that problem, and adds tests that should get the tests close
    to 100% coverage.

--- src/QuestionSet/QuestionSet-test.js
+++ src/QuestionSet/QuestionSet-test.js
@@ -1,7 +1,7 @@
 import expect from 'expect'
 import React from 'react'
 import {render, unmountComponentAtNode} from 'react-dom'
-import TestUtils from 'react-dom/test-utils'
+import {Simulate} from 'react-dom/test-utils'
 
 import QuestionSet from '.'
 
@@ -10,11 +10,17 @@ import QuestionSet from '.'
 // explanation of why this works.
 const awaitReRender = setTimeout
 
+const WHAT = "away from a center of gravity"
+
+const WHEN = "2018-01-07"
+
+const REASONS = ['magic', 'science', 'God', 'the system']
+
 const QUESTIONS = {
   ask: "What's up?",
   set: "what",
   then: {
-    ask: "When?",
+    ask: "When is {{what}}?",
     type: "DATE",
     set: "when",
     then: {
@@ -22,7 +28,7 @@ const QUESTIONS = {
       choices: [
         {
           text: "All the reasons",
-          set: { reasons: true },
+          set: { reasons: REASONS.join(', ') },
         },
         {
           text: "No reason",
@@ -33,15 +39,32 @@ const QUESTIONS = {
   }
 };
 
-const WHAT = "away from a center of gravity"
-
-const WHEN = "2018-01-07"
-
 const TEMPLATE = {
   what: "{{what}}",
   when: "{{when}}",
-  explanation: "{{#reasons}}All{{/reasons}}{{^reasons}}No{{/reasons}} reasons"
+  explanation: "Reasons: " +
+    "{{#reasons}}All of these: {{.}}{{/reasons}}" +
+    "{{^reasons}}None{{/reasons}}"
 }
+
+const RADIO_OPTIONS = [
+  {
+    radioOption: 0,
+    expectExplanation: explanation => {
+      expect(explanation).toEqual(
+        `Reasons: All of these: ${REASONS.join(', ')}`
+      )
+    }
+  },
+  {
+    radioOption: 1,
+    expectExplanation: explanation => {
+      expect(explanation).toEqual(
+        `Reasons: None`
+      )
+    }
+  }
+]
 
 describe('QuestionSet', () => {
   let node
@@ -62,40 +85,115 @@ describe('QuestionSet', () => {
   })
 
   context('when the user has answered the 1st question', () => {
-    it('displays the 1st & 2nd questions; not the 3rd', () => {
+    it('displays the 1st & 2nd questions; not the 3rd', done => {
       render(<QuestionSet questions={QUESTIONS} template={TEMPLATE}/>, node, () => {
         const what = node.querySelector('input')
         what.value = WHAT
-        TestUtils.Simulate.change(what);
+        Simulate.change(what);
 
         awaitReRender(() => {
-          expect(node.innerHTML).toContain(QUESTIONS.ask)
-          expect(node.innerHTML).toContain(QUESTIONS.then.ask)
-          expect(node.innerHTML).toNotContain(QUESTIONS.then.then.ask)
+          expect(node.innerHTML).toContain("What's up?")
+          expect(node.innerHTML).toContain(`When is ${WHAT}?`)
+          expect(node.innerHTML).toNotContain("Why?")
+          done()
         })
       })
     })
   })
 
   context('when the user has answered the 1st & 2nd questions', () => {
-    it('displays the 1st, 2nd, & 3rd questions', () => {
+    it('displays the 1st, 2nd, & 3rd questions', done => {
       render(<QuestionSet questions={QUESTIONS} template={TEMPLATE}/>, node, () => {
         const what = node.querySelector('input')
         what.value = WHAT
-        TestUtils.Simulate.change(what);
+        Simulate.change(what);
 
         awaitReRender(() => {
           const when = node.querySelectorAll('input')[1]
           when.value = WHEN
-          TestUtils.Simulate.change(what);
+          Simulate.change(when);
 
           awaitReRender(() => {
-            expect(node.innerHTML).toContain(QUESTIONS.ask)
-            expect(node.innerHTML).toContain(QUESTIONS.then.ask)
-            expect(node.innerHTML).toContain(QUESTIONS.then.then.ask)
+            expect(node.innerHTML).toContain("What's up?")
+            expect(node.innerHTML).toContain(`When is ${WHAT}?`)
+            expect(node.innerHTML).toContain("Why?")
+            done()
           })
         })
       })
+    })
+  })
+
+  context('when the user has answered all 3 questions', () => {
+    it('displays the save button', done => {
+      render(<QuestionSet questions={QUESTIONS} template={TEMPLATE}/>, node, () => {
+        const what = node.querySelector('input')
+        what.value = WHAT
+        Simulate.change(what);
+
+        awaitReRender(() => {
+          const when = node.querySelectorAll('input')[1]
+          when.value = WHEN
+          Simulate.change(when);
+
+          awaitReRender(() => {
+            const yes = node.querySelector('input[type="radio"]')
+            yes.click()
+            Simulate.change(yes)
+
+            awaitReRender(() => {
+              expect(node.innerHTML).toContain('Save')
+              done()
+            })
+          })
+        })
+      })
+    })
+  })
+
+  RADIO_OPTIONS.forEach(({radioOption, expectExplanation}) => {
+    it(`passes a correctly-filled template to onSave, option ${radioOption}`, done => {
+      let filledTemplate
+
+      const saveSpy = args => {
+        filledTemplate = args
+      }
+
+      render(
+        <QuestionSet
+          questions={QUESTIONS}
+          template={TEMPLATE}
+          onSave={saveSpy}
+        />,
+        node,
+        () => {
+          const what = node.querySelector('input')
+          what.value = WHAT
+          Simulate.change(what);
+
+          awaitReRender(() => {
+            const when = node.querySelectorAll('input')[1]
+            when.value = WHEN
+            Simulate.change(when);
+
+            awaitReRender(() => {
+              const option = node.querySelectorAll('input[type="radio"]')[radioOption]
+              option.click()
+              Simulate.change(option)
+
+              awaitReRender(() => {
+                const save = node.querySelector('input[type="submit"]')
+                save.click()
+                expect(filledTemplate.what).toEqual(WHAT)
+                expect(filledTemplate.when).toEqual(WHEN)
+                expectExplanation(filledTemplate.explanation)
+
+                done()
+              })
+            })
+          })
+        }
+      )
     })
   })
 })
